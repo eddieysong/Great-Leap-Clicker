24436f9d308325fb93988f51e9930c6ee935bee8
Eddie Song <eddie.y.song@gmail.com>
2017-06-02 19:29:38 -0400

    Added title screen (new scene) with simple animation

---
+++ Assets/BackgroundShiftScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundShiftScript : MonoBehaviour {
+
+	[SerializeField]
+	private float maxShiftX, maxShiftY, maxSpeed = 1, changeDirectionTime = 3;
+
+	private Vector2 originalPos, targetPos;
+
+	// Use this for initialization
+	void Start () {
+		originalPos = transform.position;
+		StartCoroutine (ChangeDirection ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector2.MoveTowards (transform.position, targetPos, maxSpeed/60);
+	}
+
+	IEnumerator ChangeDirection() {
+		while (true) {
+			targetPos.x = Random.Range (originalPos.x - maxShiftX, originalPos.x + maxShiftX);
+			targetPos.y = Random.Range (originalPos.y - maxShiftY, originalPos.y + maxShiftY);
+			Debug.Log (targetPos);
+			yield return new WaitForSeconds (changeDirectionTime);
+		}
+	}
+}
---
+++ Assets/RandomAlphaScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAlphaScript : MonoBehaviour {
+
+	[SerializeField]
+	private float minAlpha = 0.4f, maxAlpha = 0.8f, changeSpeed = 1f, changeTime = 3f;
+
+	private SpriteRenderer spRenderer;
+	private float targetAlpha;
+
+	// Use this for initialization
+	void Start () {
+		spRenderer = GetComponent<SpriteRenderer> ();
+		StartCoroutine (ChangeTargetAlpha ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Color newColor = spRenderer.color;
+		newColor.a += spRenderer.color.a < targetAlpha ? changeSpeed / 60 : -changeSpeed / 60;
+		spRenderer.color = newColor;
+	}
+
+	IEnumerator ChangeTargetAlpha() {
+		while (true) {
+			targetAlpha = Random.Range (minAlpha, maxAlpha);
+			yield return new WaitForSeconds (changeTime);
+		}
+	}
+}
